182ac3b9507dd720f704bf47f62567bbb1ea3ee5
YuuKohaku <soul.of.bytes@gmail.com>
2016-08-04 19:20:10 +0700

    1

--- src/externals/SAModels/Terminal.js
+++ src/externals/SAModels/Terminal.js
@@ -3,8 +3,8 @@
 let DatabaseFieldset = require("./DatabaseFieldset");
 
 class Terminal extends DatabaseFieldset {
-	static get fields(){
-		return  ["default_agent", 'attached_to', "device_type", "bound_service_groups", "occupied_by", "prebook_autoregister", "booking_methods", "reload_interval"];
+	static get fields() {
+		return ["default_agent", 'attached_to', "device_type", "bound_service_groups", "occupied_by", "prebook_autoregister", "booking_methods", "reload_interval", "ticket_design"];
 	}
 	static get references() {
 		return ['attached_to', 'default_agent', "bound_service_groups", "occupied_by"];
