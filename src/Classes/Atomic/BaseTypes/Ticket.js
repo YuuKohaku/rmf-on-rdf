3179e9f646483ff0cf5dc485012849a9f4cf44f5
YuuKohaku <soul.of.bytes@gmail.com>
2016-10-07 19:21:16 +0700

    tick fields

--- src/Classes/Atomic/BaseTypes/Ticket.js
+++ src/Classes/Atomic/BaseTypes/Ticket.js
@@ -4,7 +4,7 @@ let DatabaseFieldset = require("./DatabaseFieldset");
 
 class Ticket extends DatabaseFieldset {
 	static get fields() {
-		return ["inherits", "inheritance_level", 'session', 'locked_fields', 'booking_method', 'source', "qa_answers", 'time_description', 'operator', 'alt_operator', 'history', 'service', "code", "destination", 'org_destination', "booking_date", "dedicated_date", "priority", "state", "user_info", "service_count", "called", "expiry"];
+		return ["inheritance_counter", "inherits", "inheritance_level", 'session', 'locked_fields', 'booking_method', 'source', "qa_answers", 'time_description', 'operator', 'alt_operator', 'history', 'service', "code", "destination", 'org_destination', "booking_date", "dedicated_date", "priority", "state", "user_info", "service_count", "called", "expiry"];
 	}
 
 	build(data) {
