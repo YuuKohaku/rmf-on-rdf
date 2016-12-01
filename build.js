1513a91385285bf9fe051e9837598c8b3c7f5b41
YuuKohaku <soul.of.bytes@gmail.com>
2016-12-01 17:02:12 +0700

    upd

--- build.js
+++ build.js
@@ -25,7 +25,7 @@ gulp.task("es6-js", function () {
 	return gulp.src(src)
 		.pipe(babel({
 			"comments": false,
-			"presets": ["es2015-node5"],
+			"presets": ["es2015"],
 			"babelrc": false,
 			"plugins": plg
 		}))
