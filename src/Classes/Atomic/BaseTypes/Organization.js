35929133031ccc5eb6cfeac43733a7d8d37f11de
YuuKohaku <soul.of.bytes@gmail.com>
2016-12-13 18:45:46 +0700

    auto_expire_time

--- src/Classes/Atomic/BaseTypes/Organization.js
+++ src/Classes/Atomic/BaseTypes/Organization.js
@@ -7,8 +7,11 @@ class Organization extends DatabaseFieldset {
 		return ["okato", "pin_code_prefix", "provides", "has_schedule", "has_unit", "unit_of", "org_timezone",
 						"booking_methods", "live_autopostpone", "live_autorestore", "live_autopostpone_count",
 						"prebook_expiration_interval", "prebook_show_interval", "prebook_label_prefix", "prebook_observe_offset",
-						"prebook_register_interval", "error_dialog_duration", "formatted_address", "mkgu_code", "qa_enabled", "auto_logout_time",
-						"auto_warmup_time", "workstation_resource_enabled", "employee_resource_enabled",
+						"prebook_register_interval", "error_dialog_duration", "formatted_address", "mkgu_code", "qa_enabled",
+						"auto_logout_time",
+						"auto_warmup_time",
+						"auto_expire_time",
+						"workstation_resource_enabled", "employee_resource_enabled",
 						 "workstation_filtering_enabled", "employee_filtering_enabled", "notification_levels",
 						"sound_theme", "max_slots_per_day", "live_priority_ordering"];
 	}
