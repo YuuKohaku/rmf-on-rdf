944190bd3d09c68183a3940c670a8e1edda153c7
YuuKohaku <soul.of.bytes@gmail.com>
2017-01-18 16:24:19 +0700

    view_mode

--- src/Classes/Atomic/BaseTypes/Reception.js
+++ src/Classes/Atomic/BaseTypes/Reception.js
@@ -6,7 +6,7 @@ class Reception extends DatabaseFieldset {
 	static get fields() {
 		return ["attached_terminal", "occupied_by", "default_agent", "attached_to",
 				 "device_sound", "device_design", "device_type", "digital_display_address",
-				 "device_placement", "state", "ticket_design"];
+				 "device_placement", "state", "ticket_design", "view_mode"];
 	}
 
 	static get references() {
