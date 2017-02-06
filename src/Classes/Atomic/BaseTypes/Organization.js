5467c92455275c8ae796322553f6c9e340ef6037
artificial-life <proteus.protocol@gmail.com>
2017-02-06 16:14:33 +0600

    upd

--- src/Classes/Atomic/BaseTypes/Organization.js
+++ src/Classes/Atomic/BaseTypes/Organization.js
@@ -5,11 +5,12 @@ let DatabaseFieldset = require("./DatabaseFieldset");
 class Organization extends DatabaseFieldset {
 	static get fields() {
 		return ["okato", "pin_code_prefix", "provides", "has_schedule", "has_unit", "unit_of", "org_timezone",
-						"booking_methods", "live_autopostpone", "live_autorestore", "live_autopostpone_count",
-						"prebook_expiration_interval", "prebook_show_interval", "prebook_label_prefix", "prebook_observe_offset",
-						"prebook_register_interval", "error_dialog_duration", "formatted_address", "mkgu_code", "qa_enabled", "auto_logout_time",
-						"auto_warmup_time", "workstation_resource_enabled", "employee_resource_enabled", "notification_levels",
-						"sound_theme"];
+			"booking_methods", "live_autopostpone", "live_autorestore", "live_autopostpone_count",
+			"prebook_expiration_interval", "prebook_show_interval", "prebook_label_prefix", "prebook_observe_offset",
+			"prebook_register_interval", "error_dialog_duration", "formatted_address", "mkgu_code", "qa_enabled", "auto_logout_time",
+			"auto_warmup_time", "workstation_resource_enabled", "employee_resource_enabled", "notification_levels",
+			"sound_theme", "frontend_cache"
+		];
 	}
 
 	static get references() {
